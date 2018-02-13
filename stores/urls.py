4eca39a1a796d9ce98de0d38390dfa95bc665895
DESKTOP-3IA1VJ4\legna <ksh951129@gmail.com>
2018-02-13 21:10:28 +0900

    product_list URL 추가

--- stores/urls.py
+++ stores/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-
+    path('<int:Store_id>/', views.product_list, name='product_list')
 ]
--- stores/views.py
+++ stores/views.py
@@ -6,8 +6,13 @@ from .models import Product
 # Create your views here.
 
 def index(request):
-    store_list = Store.objects[1:3]
+    store_list = Store.objects.all()
     output = ','.join([q.store_name for q in store_list])
+    return HttpResponse(output)
+
+def product_list(request, Store_id):
+    products = Product.objects.filter(store_id=Store_id)
+    output= ','.join([q.product_name for q in products])
     return HttpResponse(output)
 
 
